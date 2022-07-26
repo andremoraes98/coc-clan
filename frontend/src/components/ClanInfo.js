d660ef9bd438c934f3b328783d1d3fb7d666249d
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-26 09:40:45 -0300

    Atualização das informações do Clan

--- frontend/src/components/ClanInfo.js
+++ frontend/src/components/ClanInfo.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import { mockResponseAPI } from '../helpers/JSON';
+import React, { useContext } from 'react';
+import ClanContext from '../context/ClanContext';
 
 const ClanInfo = () => {
-  const {
-    warWins,
-    warWinStreak,
-    members,
-    type,
-    requiredTrophies,
-    warFrequency,
-    location: { name },
-  } = mockResponseAPI;
+  const { clanInfo: {
+      warWins,
+      warWinStreak,
+      members,
+      type,
+      requiredTrophies,
+      warFrequency,
+      location,
+    },
+    isFetchOk,
+  } = useContext(ClanContext);
+
+  console.log(location)
 
   return (
-    <div>
-      <p>Guerras vencidas: { warWins }</p>
-      <p>Vitórias seguidas: { warWinStreak }</p>
-      <p>Membros: { members }</p>
-      <p>Tipos: { type }</p>
-      <p>Troféus necessários: { requiredTrophies }</p>
-      <p>Frequência de guerra: { warFrequency }</p>
-      <p>Localização do clã: { name }</p>
-    </div>
+    isFetchOk
+      ? (
+        <div>
+          <p>Guerras vencidas: { warWins }</p>
+          <p>Vitórias seguidas: { warWinStreak }</p>
+          <p>Membros: { members }</p>
+          <p>Tipos: { type }</p>
+          <p>Troféus necessários: { requiredTrophies }</p>
+          <p>Frequência de guerra: { warFrequency }</p>
+          <p>Localização do clã: { location.name }</p>
+        </div>
+      )
+      : <h1>Loading...</h1>
   )
 }
 
